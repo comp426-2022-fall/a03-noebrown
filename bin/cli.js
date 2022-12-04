3c5b17972a1a6f89ecaef2b35eddc361db92dc22
noebrown <noejbrown@gmail.com>
2022-12-04 17:31:10 -0500

    updated cli.js file

--- bin/cli.js
+++ bin/cli.js
@@ -4,3 +4,23 @@
 // imports
 import minimist from 'minimist';
 import { roll } from '../lib/roll.js';
+
+// setting values for vars
+var sides = 6;
+var dice = 2;
+var rolls = 1;
+
+// changes if args. true
+if (args.sides) {
+    var sides = args.sides
+}
+
+if(args.dice) {
+    var dice = args.dice
+}
+
+if(args.rolls) {
+    var rolls = args.rolls
+}
+
+console.log(roll(sides, dice, rolls))
